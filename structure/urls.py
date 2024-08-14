03ee1ccedf3ccfc31cd333207de8c3172d33176b
Luis Taracena <lpablotaracena@gmail.com>
2024-08-14 17:57:39 +0200

    UPDATE: Structure Blast feature
    UPDATE: Structure_blast
    
    UPDATE: Structure Blast
    
    REVERT
    
    UPDATE: Adding Structure Blast index

--- structure/urls.py
+++ structure/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from structure.views import *
 from structure import views
 from django.conf import settings
@@ -11,6 +12,7 @@ urlpatterns = [
     url(r'^g_protein_structure_browser$', cache_page(60*60*24)(EffectorStructureBrowser.as_view(effector='gprot')), name='g_protein_structure_browser'),
     # url(r'^g_protein_structure_browser$', EffectorStructureBrowser.as_view(effector='gprot'), name='g_protein_structure_browser'),
     url(r'^arrestin_structure_browser$', cache_page(60*60*24)(EffectorStructureBrowser.as_view(effector='arrestin')), name='arrestin_structure_browser'),
+    path('structure_blast', StructureBlastView.as_view(), name='structure-blast'),
 
     url(r'^browser$', RedirectBrowser, name='redirect_browser'),
     url(r'^selection_convert$', ConvertStructuresToProteins, name='convert'),
